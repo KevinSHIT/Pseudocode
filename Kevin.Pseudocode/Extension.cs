dea471767f2e2afbd4955a920372d433d772b4c3
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-02-28 23:17:35 +0000

    淦！我转换器又炸了！

--- Kevin.Pseudocode/Extension.cs
+++ Kevin.Pseudocode/Extension.cs
@@ -9,8 +9,17 @@ namespace Kevin.Pseudocode
         public static double TO_DOUBLE(this object obj)
             => double.Parse(obj.ToString() ?? string.Empty);
 
+        public static double TO_DOUBLE(this bool obj)
+            => obj.TO_INT();
+
         public static int TO_INT(this bool obj)
             => obj ? 1 : 0;
+
+        public static int TO_INT(this double obj)
+            => (int) obj;
+
+        public static int TO_INT(this float obj)
+            => (int) obj;
 
         public static int TO_INT(this object obj)
             => int.Parse(obj.ToString() ?? string.Empty);
